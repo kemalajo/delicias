e9071a5e5a34bed2dbb97379acd65ea5d0bc8bc4
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2025-09-18 14:00:42 -0300

    Add files via upload

---
+++ delicias/src/main/java/com/senai/delicias/DeliciasApplication.java
@@ -0,0 +1,11 @@
+package com.senai.delicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliciasApplication.class, args);
+    }
+}
