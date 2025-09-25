a03ccf8964e38d4ab0d982b51d4ced26ec38769d
kerenStevaux <keren.anastacio@gmail.com>
2025-09-25 09:32:16 -0300

    Criação dos produtos
    No service, controller e repository

---
+++ delicias/src/main/java/com/senai/delicias/service/ProdutoService.java
@@ -0,0 +1,34 @@
+package com.senai.delicias.service;
+
+import com.senai.delicias.entities.Produto;
+import com.senai.delicias.repositories.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public Produto save(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void delete(Long id) {
+        produtoRepository.deleteById(id);
+    }
+}
