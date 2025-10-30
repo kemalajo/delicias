b68d7de4ee03871bc1bacf32b98b18fb6e601f46
kerenStevaux <keren.anastacio@gmail.com>
2025-10-30 07:29:55 -0300

    Ajudes na importação do produto entidade

--- delicias/delicias/src/main/java/com/senai/delicias/entities/Produto.java
+++ delicias/delicias/src/main/java/com/senai/delicias/entities/Produto.java
@@ -1,8 +1,13 @@
 package com.senai.delicias.entities;
 
-import java.util.List;
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_produtos") // use sempre minúsculas, por consistência
